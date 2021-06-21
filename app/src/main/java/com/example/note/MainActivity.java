0fbc8520ef83acde0ee701dfe87ebef50302b586
Oleg <pronin.o.b@yandex.ru>
2021-06-21 11:24:52 +0300

    Разработка приложения заметок

--- app/src/main/java/com/example/note/MainActivity.java
+++ app/src/main/java/com/example/note/MainActivity.java
@@ -11,33 +11,9 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    String[] name = { "Иван", "Марья", "Петр", "Антон", "Даша", "Борис",
-            "Костя", "Игорь" , "Иван", "Марья", "Петр", "Антон", "Даша", "Борис",
-            "Костя", "Игорь" };
-    String[] position = { "Программер", "Бухгалтер", "Программер",
-            "Программер", "Бухгалтер", "Директор", "Программер", "Охранник" ,"Программер", "Бухгалтер", "Программер",
-            "Программер", "Бухгалтер", "Директор", "Программер", "Охранник"};
-    int salary[] = { 13000, 10000, 13000, 13000, 10000, 15000, 13000, 8000, 13000, 10000, 13000, 13000, 10000, 15000, 13000, 8000  };
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        LinearLayout linLayout = (LinearLayout) findViewById(R.id.linLayout);
-
-        LayoutInflater ltInflater = getLayoutInflater();
-
-        for (int i = 0; i < name.length; i++) {
-            Log.d("myLogs", "i = " + i);
-            View item = ltInflater.inflate(R.layout.item, linLayout, false);
-            TextView tvName = (TextView) item.findViewById(R.id.listNoteTitle);
-            tvName.setText(name[i]);
-            TextView tvPosition = (TextView) item.findViewById(R.id.listNotePrev);
-            tvPosition.setText("Должность: " + position[i]);
-            TextView tvSalary = (TextView) item.findViewById(R.id.listNoteDate);
-            tvSalary.setText("Оклад: " + String.valueOf(salary[i]));
-            linLayout.addView(item);
-        }
     }
 }
--- app/src/main/java/com/example/note/entity/Note.java
+++ app/src/main/java/com/example/note/entity/Note.java
@@ -1,8 +1,11 @@
 package com.example.note.entity;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
 import java.util.Date;
 
-public class Note {
+public class Note implements Parcelable {
     private String name;
     private String description;
     private Date createDate;
@@ -13,30 +16,56 @@ public class Note {
         this.createDate = createDate;
     }
 
+    protected Note(Parcel in) {
+        name = in.readString();
+        description = in.readString();
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(name);
+        dest.writeString(description);
+        dest.writeValue(createDate);
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    public static final Creator<Note> CREATOR = new Creator<Note>() {
+        @Override
+        public Note createFromParcel(Parcel in) {
+            return new Note(in);
+        }
+
+        @Override
+        public Note[] newArray(int size) {
+            return new Note[size];
+        }
+    };
+
     public String getName() {
         return name;
     }
 
-    public Note setName(String name) {
+    public void setName(String name) {
         this.name = name;
-        return this;
     }
 
     public String getDescription() {
         return description;
     }
 
-    public Note setDescription(String description) {
+    public void setDescription(String description) {
         this.description = description;
-        return this;
     }
 
     public Date getCreateDate() {
         return createDate;
     }
 
-    public Note setCreateDate(Date createDate) {
+    public void setCreateDate(Date createDate) {
         this.createDate = createDate;
-        return this;
     }
 }
